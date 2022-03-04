72436c7273f4b26e6ff67b0d785acae32526ba97
Ryan Friesen <viniloruido@gmail.com>
2022-03-03 18:54:47 -0800

    setup algo reqs

---
+++ arrays/countEvenNegatives/countEvenNegatives.js
@@ -0,0 +1,11 @@
+const nums1 = [1, 5, -1, 2, -4, 9, -10, 0, -3, -2];
+const expected1 = 3;
+
+/**
+ * Counts how many numbers are both even and negative.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {number} nums
+ * @returns {number} The count.
+ */
+function countEvenNegatives(nums) {}
