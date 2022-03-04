b484f71044c5c91db4d7edf94b3c337c6b3f9cee
Ryan Friesen <viniloruido@gmail.com>
2022-03-03 19:08:38 -0800

    tbd test cases

---
+++ arrays/shift/tbd.js
@@ -0,0 +1,18 @@
+const { expect } = require('@jest/globals');
+const { shift } = require('./shift');
+
+const arr2 = ['a', 'b', 'c', 'd'];
+const expected2 = 'a';
+const arr2Expected = ['b', 'c', 'd'];
+
+test('remove the first item of the given array and shift down the remaining items', () => {
+  const arr1 = [1, 2, 3];
+  const expected1 = 1;
+  // after running function arr1 should now be:
+  const arr1Expected = [2, 3];
+
+  const actual = shift(arr1);
+
+  // Check that the the given array was returned instead of a new array.
+  expect(actual).toBe(arr1);
+});
