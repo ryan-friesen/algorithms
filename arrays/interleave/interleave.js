557674d2f68f58a2a238f0ba711b12f7934a0404
Ryan Friesen <viniloruido@gmail.com>
2022-03-03 18:54:36 -0800

    add o notation comments

--- arrays/interleave/interleave.js
+++ arrays/interleave/interleave.js
@@ -4,6 +4,8 @@ const b = [4, 5, 6, 7, 8];
 /**
  * Interleaves the two given arrays in a zipper like fashion. The longer
  * array's items will be added to the back.
+ * - Time: O(n + m) linear. n = a.length, m = b.length.
+ * - Space: O(n + m).
  * @param {any[]} a The first array to begin interleaving from.
  * @param {any[]} b The second array.
  * @returns {any[]} A new array containing the interleaved values.
