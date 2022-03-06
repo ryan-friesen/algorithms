2cb0f2291170e93e71e81c289c950119b135f98a
Ryan Friesen <viniloruido@gmail.com>
2022-03-05 16:31:43 -0800

    Refactored (removed sort method to reduce calc cost)

--- src/arrays/countEvenNegatives/index.js
+++ src/arrays/countEvenNegatives/index.js
@@ -1,6 +1,7 @@
 const nums1 = [1, 5, -1, 2, -4, 9, -10, 0, -3, -2];
 const expected1 = 3;
-
+const nums2 = [10, 0, 3, 2];
+const expected2 = 0;
 /**
  * Counts how many numbers are both even and negative.
  * - Time: O(?).
@@ -8,20 +9,15 @@ const expected1 = 3;
  * @param {number} nums
  * @returns {number} The count.
  */
-function countEvenNegatives(nums1) {
-  console.log(nums1);
-  let sortedArray = nums1.sort((a, b) => a - b);
-  let negativeEvenCount = [];
-  for (i = 0; i < sortedArray.length; i++ ) {
-    if(sortedArray[i] % 2 == 0 && sortedArray[i] < 0) {
-      negativeEvenCount.push(sortedArray[i]);
+function countEvenNegatives(nums) {
+  let negativeEvenCount = 0;
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 == 0 && nums[i] < 0) {
+      negativeEvenCount++;
     }
   }
-  console.log(sortedArray);
-  console.log(negativeEvenCount);
+  return negativeEvenCount;
 }
-
-countEvenNegatives(nums1);
 
 module.exports = {
   countEvenNegatives,
