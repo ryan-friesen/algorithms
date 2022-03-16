a44c568aa7b53fedf31edd5aed2c5decadfd3701
Ryan Friesen <viniloruido@gmail.com>
2022-03-16 16:31:21 -0700

    Added .spec test file for countEvenNegatives function

---
+++ src/arrays/countEvenNegatives/index.spec.js
@@ -0,0 +1,8 @@
+const { countEvenNegatives } = require('.');
+
+describe('Counts the even negative numbers in an array', () => {
+    it('should return [-2, -4, -6]', () => {
+        expect(countEvenNegatives([-2, -6, 4, -1, 9, 3]))
+            .toEqual(2)
+    })
+})
