2eac2f02baef5cad854b9d2cb6f312132e84fb3f
Ryan Friesen <viniloruido@gmail.com>
2022-03-16 17:44:50 -0700

    format

--- src/arrays/countEvenNegatives/index.spec.js
+++ src/arrays/countEvenNegatives/index.spec.js
@@ -1,8 +1,7 @@
 const { countEvenNegatives } = require('.');
 
 describe('Counts the even negative numbers in an array', () => {
-    it('should return [-2, -4, -6]', () => {
-        expect(countEvenNegatives([-2, -6, 4, -1, 9, 3]))
-            .toEqual(2)
-    })
-})
+  it('should return [-2, -4, -6]', () => {
+    expect(countEvenNegatives([-2, -6, 4, -1, 9, 3])).toEqual(2);
+  });
+});
