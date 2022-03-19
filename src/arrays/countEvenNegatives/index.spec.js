5fd5f04948615bca6924c1fe5a9ffedd3d7ef02e
Ryan Friesen <viniloruido@gmail.com>
2022-03-19 15:08:14 -0700

    Refactored test cases to match the slice array test cases

--- src/arrays/countEvenNegatives/index.spec.js
+++ src/arrays/countEvenNegatives/index.spec.js
@@ -1,7 +1,34 @@
 const { countEvenNegatives } = require('.');
 
-describe('Counts the even negative numbers in an array', () => {
-  it('should return [-2, -4, -6]', () => {
-    expect(countEvenNegatives([-2, -6, 4, -1, 9, 3])).toEqual(2);
-  });
+const arr1 = [-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5];
+const expected1 = 2;
+
+const arr2 = [-5, -3, -2, -10];
+const expected2 = 2;
+
+const testCases = [
+  {
+    args: arr1,
+    expected: expected1,
+  },
+  {
+    args: arr2,
+    expected: expected2
+  }
+
+];
+
+describe('count even negatives', () => {
+  testCases.forEach((testCase, i) => {
+    const {
+      args,
+      expected,
+    } = testCase;
+
+    describe(`test case ${i}`, () => {
+      it(`should return ${expected}`, () => {
+        expect(countEvenNegatives(args)).toEqual(expected);
+      })
+    })
+  })
 });
