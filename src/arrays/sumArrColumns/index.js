b69b583ca5f2756ec856cda24dca04559876aa86
Ryan Friesen <viniloruido@gmail.com>
2022-03-22 18:17:54 -0700

    format

--- src/arrays/sumArrColumns/index.js
+++ src/arrays/sumArrColumns/index.js
@@ -12,36 +12,17 @@ const expected1 = [30, 40, 50];
  * @returns {Array<number>} The column sums.
  */
 function sumArrColumns(nums1, nums2) {
-    let sumArr = [];
-    for (let i = 0; i < nums1.length; i++) {
-        sumArr[i] = nums1[i] + nums2[i];
+  let sumArr = [];
+  for (let i = 0; i < nums1.length; i++) {
+    sumArr[i] = nums1[i] + nums2[i];
+  }
+  return sumArr;
+}
 
-    }
-    return sumArr;
-};
-
-
-const sumArrColumnsFunctional = (nums1, nums2) => nums1.map((num1, i) => num1 + nums2[i]);
-
-// function ourMap(arr, cb) {
-//     const newArr = [];
-
-//     for (let i = 0; i < arr.length; i++) {
-//         const element = arr[i];
-//         const newElement = cb(element, i);
-//         newArr.push(newElement);
-//     }
-
-//     return newArr;
-// }
-
-// const transformedArr = ourMap(['a', 'aa', 'aaa'], function (item, idx) { return item.slice(0, 1).toUpperCase() });
-
-// console.log(transformedArr);
-
-// console.log(sumArrColumnsFunctional(numbersA1, numbersB1));
+const sumArrColumnsFunctional = (nums1, nums2) =>
+  nums1.map((num1, i) => num1 + nums2[i]);
 
 module.exports = {
-    sumArrColumns: sumArrColumns,
-    sumArrColumnsFunctional: sumArrColumnsFunctional
-}
+  sumArrColumns: sumArrColumns,
+  sumArrColumnsFunctional: sumArrColumnsFunctional,
+};
