71e016d0745a360f601d02fd697f2837545c76ab
Ryan Friesen <viniloruido@gmail.com>
2022-03-22 18:15:59 -0700

    format and remove redundant test case

--- src/arrays/sumArrColumns/index.spec.js
+++ src/arrays/sumArrColumns/index.spec.js
@@ -1,14 +1,13 @@
 const { sumArrColumns, sumArrColumnsFunctional } = require('.');
 
 describe('sumArrColumns', () => {
-    it("should return the sum of two same-length arrays", () =>
-        expect(sumArrColumns([1, 2, 3], [11, 12, 13])).toEqual([12, 14, 16]));
-    it("should throw an error", () => {
-        expect(sumArrColumns([1, 2, 3], [4, 5, undefined]).toEqual([5, 7, NaN]));
-    })
-})
+  it('should return the sum of two same-length arrays', () =>
+    expect(sumArrColumns([1, 2, 3], [11, 12, 13])).toEqual([12, 14, 16]));
+});
 
-describe('sumArrColunsFunctional', () => {
-    it("should return the sum of arrays using functional syntax", () =>
-        expect(sumArrColumnsFunctional([2, 4, 6], [22, 44, 66])).toEqual([24, 48, 72]));
-})
+describe('sumArrColumnsFunctional', () => {
+  it('should return the sum of arrays using functional syntax', () =>
+    expect(sumArrColumnsFunctional([2, 4, 6], [22, 44, 66])).toEqual([
+      24, 48, 72,
+    ]));
+});
