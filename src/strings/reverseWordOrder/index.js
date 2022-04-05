06058da8a46a403081c3e8aee4f302c29b0c6a74
Ryan Friesen <viniloruido@gmail.com>
2022-04-04 18:11:15 -0700

    added reverseWordOrder function logic with decrementing for loop and global 1st argument in replace method

--- src/strings/reverseWordOrder/index.js
+++ src/strings/reverseWordOrder/index.js
@@ -7,7 +7,15 @@
  * @returns {string} The given string with the word order reversed but the words
  *    themselves are not reversed.
  */
-function reverseWordOrder(wordsStr) {}
+
+function reverseWordOrder(wordsStr) {
+  const stringArr = wordsStr.split(" ");
+  let reversedArr = [];
+  for (i = stringArr.length; i > 0; i--) {
+    reversedArr.push(stringArr[i - 1]);
+  }
+  return reversedArr.toString().replace(/,/g, ' ');
+}
 
 module.exports = {
   reverseWordOrder: reverseWordOrder,
